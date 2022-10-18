eb5130a286b725aa1e6af268d5e86f3680ed5187
chriscerque <christophe.cerqueira741@gmail.com>
2022-10-18 15:25:29 +0200

    init

---
+++ src/main/java/net/ent/etrs/gestionstagiaire/GestionstagiaireApplication.java
@@ -0,0 +1,13 @@
+package net.ent.etrs.gestionstagiaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionstagiaireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionstagiaireApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/ent/etrs/gestionstagiaire/GestionstagiaireApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ent.etrs.gestionstagiaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionstagiaireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
