40038b3153ffce930ed6a1efc8cc53f14d4440e8
chriscerque <christophe.cerqueira741@gmail.com>
2022-10-19 17:18:38 +0200

    init

--- src/main/java/net/ent/etrs/gestionstagiaire/secu/AuditorAwareImpl.java
+++ src/main/java/net/ent/etrs/gestionstagiaire/secu/AuditorAwareImpl.java
@@ -4,6 +4,7 @@ import net.ent.etrs.gestionstagiaire.model.entities.MyUser;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -34,9 +35,12 @@ public class AuditorAwareImpl implements AuditorAware<String> {
         if (authentication.getPrincipal() instanceof MyUser) {
             System.out.println("((User) authentication.getPrincipal()).getUsername() : " + ((MyUser) authentication.getPrincipal()).getUsername());
             return Optional.of(((MyUser) authentication.getPrincipal()).getUsername());
+        } else {
+            System.out.println("authentication.getClass() : " + authentication.getClass());
+            return Optional.of(((AnonymousAuthenticationToken) authentication.getPrincipal()).getName());
         }
-        System.out.println("authentication 33");
-        return Optional.of("ANONYMOUS");
+//        System.out.println("authentication 33");
+//        return Optional.of("ANONYMOUS");
 //        return Optional.of(((MyUser) new AnonymousAuthenticationFilter("Anonymous").getPrincipal()).getUsername());
     }
 }
